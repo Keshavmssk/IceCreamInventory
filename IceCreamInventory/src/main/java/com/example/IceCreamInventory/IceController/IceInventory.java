15277f4123d559250efca7bdad8992e693f8280d
Keshavmssk <keshavs.ip.20@nitj.ac.in>
2023-08-01 07:56:20 +0530

    first commit

---
+++ IceCreamInventory/src/main/java/com/example/IceCreamInventory/IceController/IceInventory.java
@@ -0,0 +1,14 @@
+package com.example.IceCreamInventory.IceController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IceInventory 
+{	
+	@GetMapping("/index")
+	   public String index() 
+           {
+	      return "index";
+	   }
+}
